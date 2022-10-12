0756c0b54e5a694a2aff9408e3b648d5e2333c9c
GonzaloSoria <gonzalo.sooria@gmail.com>
2022-10-12 15:56:00 -0300

    add menu data

--- src/data.js
+++ src/data.js
@@ -13,5 +13,69 @@ export const data = {
             title: "Suscribite",
             url: "#"
         }]
+    },
+    menu: {
+        rolls: [{
+            title: "California Roll",
+            description: "Salmón, Philadelphia, Palta y Sésamo",
+            price: "$1200",
+            units: "12 Unidades",
+            img: "../img/california-roll.png"
+        },{
+            title: "Smoke Roll",
+            description: "Philadelphia, Palta y Salmón",
+            price: "$1000",
+            units: "12 Unidades",
+            img: "../img/smoked-roll.png"
+        },{
+            title: "Spicy Roll",
+            description: "Kanikama, Phila, bañado en Ají",
+            price: "$1350",
+            units: "12 Unidades",
+            img: "../img/spicy-roll.png"
+        },{
+            title: "California Roll",
+            description: "Salmón, Philadelphia, Palta y Sésamo",
+            price: "$1200",
+            units: "12 Unidades",
+            img: "../img/california-roll.png"
+        },{
+            title: "California Roll",
+            description: "Salmón, Philadelphia, Palta y Sésamo",
+            price: "$1200",
+            units: "12 Unidades",
+            img: "../img/california-roll.png"
+        },{
+            title: "California Roll",
+            description: "Salmón, Philadelphia, Palta y Sésamo",
+            price: "$1200",
+            units: "12 Unidades",
+            img: "../img/california-roll.png"
+        },{
+            title: "California Roll",
+            description: "Salmón, Philadelphia, Palta y Sésamo",
+            price: "$1200",
+            units: "12 Unidades",
+            img: "../img/california-roll.png"
+        },{
+            title: "California Roll",
+            description: "Salmón, Philadelphia, Palta y Sésamo",
+            price: "$1200",
+            units: "12 Unidades",
+            img: "../img/california-roll.png"
+        }],
+        soap: [{
+            title: "California Roll",
+            description: "Salmón, Philadelphia, Palta y Sésamo",
+            price: "$1200",
+            units: "12 Unidades",
+            img: "../img/california-roll.png"
+        },{
+            title: "California Roll",
+            description: "Salmón, Philadelphia, Palta y Sésamo",
+            price: "$1200",
+            units: "12 Unidades",
+            img: "../img/california-roll.png"
+        }]
     }
 }
